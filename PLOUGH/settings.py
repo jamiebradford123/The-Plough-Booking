f21974e91187e9cc9263ce6538964ba2841ec455
jamiebradford123 <jamiebradford123@hotmail.com>
2023-01-23 13:11:49 +0000

    deployment commit

--- PLOUGH/settings.py
+++ PLOUGH/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-plough.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['the-plough.herokuapp.com', 'localhost']
 
 
 # Application definition
