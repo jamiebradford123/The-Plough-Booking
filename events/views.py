c47cf3e2a03a566737eb2bb36260e90fd33ff714
jamiebradford123 <jamiebradford123@hotmail.com>
2023-01-26 18:11:59 +0000

    add template

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.filter(status=1).order_by('created_on')
+    template_name = 'event.html'
+    paginate_by = 6
