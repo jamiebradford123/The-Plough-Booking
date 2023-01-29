10cb424a7d450d14df64c63bd853d7b63c2ca59b
jamiebradford123 <jamiebradford123@hotmail.com>
2023-01-29 15:39:16 +0000

    add event detail page

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('event', views.EventList.as_view(), name='event')
+    path('event', views.EventList.as_view(), name='event'),
+    path('<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 
@@ -8,3 +8,23 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('created_on')
     template_name = 'event.html'
     paginate_by = 6
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        comments = event.comments.filter(approved=True).order_by('created_on')
+        interested = False
+        if event.interested.filter(id=self.request.user.id).exists():
+            intersted = True
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": event,
+                "comments": comments,
+                "interested": interested
+            }
+        )
