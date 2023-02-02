5c6b26cbb0ff4de063059f45612e767f5a8e72e6
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-02 11:56:44 +0000

    add booking model

--- book/admin.py
+++ book/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import Book
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'approved', 'created_on', 'date')
+    search_fields = ['name', 'date']
+    actions = ['approve_booking']
+
+    def approve_booking(self, request, queryset):
+        queryset.update(approved=True)
--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    number_of_guests = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    date = models.DateTimeField()
+    email = models.EmailField()
+    requests = models.TextField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return f"{self.date} for {self.name}"
