9b81617f05a3eedc9366d39c4e1ed36c601998fd
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-02 15:30:11 +0000

    ensure sign in is needed before booking

--- book/views.py
+++ book/views.py
@@ -4,11 +4,20 @@ from .models import Book
 from .forms import BookForm
 
 
+from django.http import HttpResponseRedirect
+
+
 def Book(request):
     """
     Renders the book page
     """
-
+    if request.user.is_authenticated:
+        form = BookForm(request.POST or None)
+        context = {
+            "form": form,
+        }
+    else:
+        return HttpResponseRedirect("accounts/login")
     return render(
         request,
         "book.html",
