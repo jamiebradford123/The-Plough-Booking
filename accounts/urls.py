bafd87fb558ebf99351da0c14e96a8c8ca9f8c3e
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-07 13:19:33 +0000

    add edit, delete and approve buttons to bookings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('mybookings', views.mybookings, name='mybookings'),
+    path('edit/<book_id>', views.edit_book, name='editbook'),
+    path('toggle/<book_id>', views.toggle_book, name='togglebook'),
+    path('delete/<book_id>', views.delete_book, name='deletebook'),
 ]
