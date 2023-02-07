0d1e7e661ce949cd16f36cc91f651cffcc26d08f
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-07 11:45:41 +0000

    add view reservation page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from book.forms import BookForm
 from book.models import Book
 
 
 def mybookings(request):
-    return render(request, "mybookings.html")
+    booking = Book.objects.filter()
+    context = {
+            "booking": booking,
+     }
+    return render(
+        request, "mybookings.html", context)
