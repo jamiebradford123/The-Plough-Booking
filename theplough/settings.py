2202eecf53ab217b44fca658b4776b7c7b8b57ef
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-07 13:59:16 +0000

    css error

--- theplough/settings.py
+++ theplough/settings.py
@@ -20,6 +20,17 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
--- theplough/urls.py
+++ theplough/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
