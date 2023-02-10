94e656c3a6989648ee3227355f90b16bd5c98cca
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-10 18:02:30 +0000

    final commit of day

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'Admin'
+        CUSTOMER = "CUSTOMER", 'Customer'
+        STAFF = "STAFF", 'Staff'
+    
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    def save(self, *arg, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
