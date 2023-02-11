1b85165ff4eb1fa0741c444f23edf6424f199349
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-11 12:23:19 +0000

    add admin page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('mybookings', views.mybookings, name='mybookings'),
+    path('staff', views.staff, name='staff'),
     path('edit/<book_id>', views.edit_book, name='editbook'),
     path('toggle/<book_id>', views.toggle_book, name='togglebook'),
     path('delete/<book_id>', views.delete_book, name='deletebook'),
+    path('customer', views.customer, name='customer'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,16 @@ from book.forms import BookForm
 from book.models import Book
 
 
-def mybookings(request):
+def staff(request):
     booking = Book.objects.order_by('date')
     context = {
             "booking": booking,
      }
     return render(
-        request, "mybookings.html", context)
+        request, "staff.html", context)
+
+def customer(request):
+    return render(request, "customer.html")
 
 
 def edit_book(request, book_id):
@@ -31,9 +34,9 @@ def toggle_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book.approved = not book.approved
     book.save()
-    return redirect('mybookings')
+    return redirect('staff')
 
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book.delete()
-    return redirect('mybookings')
+    return redirect('staff')
