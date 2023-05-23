92baee617511d1b7a635ada097f635bfa1ed7221
jamiebradford123 <>
2023-05-23 17:19:11 +0000

    prevent events being created in the past

--- events/models.py
+++ events/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.core.validators import MinLengthValidator, MinValueValidator
 from datetime import date, datetime
+from django.core.exceptions import ValidationError
+from .validators import validate_date
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Event(models.Model):
     title = models.CharField(max_length=150, unique=True)
-    event_date = models.DateField()
+    event_date = models.DateField(validators=[validate_date])
     time = models.TimeField(null=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
