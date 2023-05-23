ccf039e6fd9ca5c9fa199cbf186eb911844c6a80
jamiebradford123 <>
2023-05-23 14:14:20 +0000

    Fix edit booking issue

--- book/urls.py
+++ book/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('book', views.book, name='book'),
-    path('success', views.book, name='success'),
-    path('fail', views.book, name='fail'),
-    path('manage_bookings', views.manage_bookings, name='managebookings'),
-    path('edit/<book_id>', views.edit_book, name='editbook'),
-    path('toggle/<book_id>', views.toggle_book, name='togglebook'),
-    path('delete/<book_id>', views.delete_book, name='deletebook'),
+    path("book", views.book, name="book"),
+    path("success", views.book, name="success"),
+    path("fail", views.book, name="fail"),
+    path("manage_bookings", views.manage_bookings, name="managebookings"),
+    path("edit/<book_id>", views.edit_book, name="editbook"),
+    path("toggle/<book_id>", views.toggle_book, name="togglebook"),
+    path("delete_book/<book_id>", views.delete_book, name="deletebook"),
 ]
